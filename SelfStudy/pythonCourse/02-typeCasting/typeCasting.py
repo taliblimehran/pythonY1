395fa0011c11762a4488dbfc44e0dbcf708f892a
taliblm1 <mehran.talibli@aalto.fi>
2024-09-14 20:33:39 +0300

    typeCasting

---
+++ SelfStudy/pythonCourse/02-typeCasting/typeCasting.py
@@ -0,0 +1,25 @@
+# Typecasting = the process of converting a variable from one data type to another
+# str(), int(), float(), bool()
+
+name = "Mehran"
+age = 29
+gpa = 4.17
+is_student = True
+
+# gpa = int(gpa)
+#
+# print(gpa)
+
+# age = float(age)
+#
+# print(age)
+
+# age = str(age)
+#
+# age += "1"
+#
+# print(age)
+
+name = bool(name)
+
+print(name)
