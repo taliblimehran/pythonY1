9ab520d4ac8da09ebe9414b0efd74e93184a4be7
taliblm1 <mehran.talibli@aalto.fi>
2024-09-14 20:13:48 +0300

    variable examples

---
+++ SelfStudy/pythonCourse/01-variables/variables.py
@@ -0,0 +1,30 @@
+# Strings
+first_name = "Mehran"
+food = "lentils"
+email = "talibli.mehran@gmail.com"
+
+print(f"hello {first_name}")
+print(f"I like {food}")
+print(f"my email is {email}")
+
+# Integers
+age = 29
+quantity = 5
+
+print(f"you are {age} years old")
+print(f"I am buying {quantity} eggs")
+
+# Float
+distance = 7.8
+print(f"I run {distance}km")
+
+# Boolean
+is_student = True
+
+if (is_student):
+    print("I am a student")
+else:
+    print(" I am not a student")
+
+
+
