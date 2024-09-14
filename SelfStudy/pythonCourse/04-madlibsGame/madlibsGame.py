1cdd1022af21e86b77de06ddbfd91fcc94305f60
taliblm1 <mehran.talibli@aalto.fi>
2024-09-14 21:44:20 +0300

    adding madlibsGame story

---
+++ SelfStudy/pythonCourse/04-madlibsGame/madlibsGame.py
@@ -0,0 +1,15 @@
+# Madlibs game
+# word game where you create a story
+# by filling in blanks with random words
+
+
+adjective1 = input("Enter an adjective (description)\n")
+noun1 = input("Enter a noun(person, place, thing)\n")
+adjective2 = input("Enter an adjective (description)\n")
+verb1 = input("Enter a verb ending'ing'\n")
+adjective3 = input("Enter an adjective (description\n")
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
