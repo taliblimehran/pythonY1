107cb5f5e948aea1376e828a914cb1dfb5cf4add
taliblm1 <mehran.talibli@aalto.fi>
2024-09-15 23:34:10 +0300

    creatinh simple calculator program

---
+++ SelfStudy/pythonCourse/07-calculatorProgram/calculatorProgram.py
@@ -0,0 +1,21 @@
+# Python calculator
+
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter the 1st number:\n "))
+num2 = float(input("Enter the 2nd number:\n "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 3))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 3))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 3))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 3))
+else:
+    print(f"{operator} is not a valid operator")
+
